ecf21f2f5f036263c922d3b98717622563bba548
Claudio Espinoza <claudioyugen@gmail.com>
2025-09-14 16:45:55 -0300

    feat: Define LicenseIssuer, LicenseRetriever, LicensesByPatientRetriever, and LicenseVerifier interfaces

--- internal/application/usecase/contrats/license_interface.go
+++ internal/application/usecase/contrats/license_interface.go
@@ -5,6 +5,22 @@ import (
 	dto "license-service/internal/application/dto"
 )
 
-type IssueLicenseUseCase interface {
+// Para POST /licenses
+type LicenseIssuer interface {
 	Execute(ctx context.Context, fermenterDTO dto.CreateLicenseDTO) (dto.LicenseDTO, error)
+}
+
+// Para GET /licenses/{folio}
+type LicenseRetriever interface {
+	Execute(ctx context.Context, folio string) (dto.LicenseDTO, error)
+}
+
+// Para GET /licenses?patientId={id}
+type LicensesByPatientRetriever interface {
+	Execute(ctx context.Context, patientID string) ([]dto.LicenseDTO, error)
+}
+
+// Para GET /licenses/{folio}/verify
+type LicenseVerifier interface {
+	Execute(ctx context.Context, folio string) (bool, error)
 }
