0091f8b8169695320a9ce34e3bf33b67f419febb
Claudio Espinoza <claudioyugen@gmail.com>
2025-09-20 19:36:25 -0300

    refactor: Update LicenseRepository and PatientRepository to use models from the correct package

--- internal/domain/repositories/license_repository.go
+++ internal/domain/repositories/license_repository.go
@@ -2,12 +2,12 @@ package repositories
 
 import (
 	"context"
-	models "license-service/internal/persistence/entities"
+	models "license-service/internal/domain/model"
 )
 
 type LicenseRepository interface {
-	Save(ctx context.Context, license *models.LicenseModel) error
-	FindByFolio(ctx context.Context, folio string) (*models.LicenseModel, error)
-	FindByPatientID(ctx context.Context, patientID string) ([]*models.LicenseModel, error)
+	Save(ctx context.Context, license *models.License) error
+	FindByFolio(ctx context.Context, folio string) (*models.License, error)
+	FindByPatientID(ctx context.Context, patientID string) ([]*models.License, error)
 	ExistsByFolioAndStatus(ctx context.Context, folio string, status string) (bool, error)
 }
--- internal/domain/repositories/patient_repository.go
+++ internal/domain/repositories/patient_repository.go
@@ -2,8 +2,10 @@ package repositories
 
 import (
 	"context"
+	model "license-service/internal/domain/model"
 )
 
 type PatientRepository interface {
 	ExistsByPatientID(ctx context.Context, patientID string) (bool, error)
+	SavePatient(ctx context.Context, patient model.Patient) error
 }
