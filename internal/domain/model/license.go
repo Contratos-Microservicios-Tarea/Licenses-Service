829298efb1904224d82abf0e8e894f418f7b4855
Claudio Espinoza <claudioyugen@gmail.com>
2025-09-20 19:37:34 -0300

    refactor: Update License struct and methods for improved clarity and functionality

--- internal/domain/model/license.go
+++ internal/domain/model/license.go
@@ -1,13 +1,21 @@
 package domain
 
 import (
+	"fmt"
+	err "license-service/pkg/log/error"
+	logger "license-service/pkg/log/logger"
 	"time"
+)
+
+const (
+	StatusIssued  = "issued"
+	StatusExpired = "expired"
+	StatusRevoked = "revoked"
 )
 
 type License struct {
 	Folio     string
-	Rut       string
-	LastName  string
+	PatientID string
 	DoctorID  string
 	Diagnosis string
 	StartDate time.Time
@@ -15,16 +23,45 @@ type License struct {
 	Days      uint8
 }
 
-func NewLicense(license License) (*License, error) {
+func NewLicense(license License) *License {
 	return &License{
 		Folio:     license.Folio,
-		Rut:       license.Rut,
-		LastName:  license.LastName,
+		PatientID: license.PatientID,
 		DoctorID:  license.DoctorID,
 		Diagnosis: license.Diagnosis,
 		StartDate: license.StartDate,
 		Status:    license.Status,
 		Days:      license.Days,
-	}, nil
+	}
+}
 
+func ValidateDays(days uint8) error {
+	if days <= 0 {
+		AppError := err.NewAppError(err.ErrInternalError, "license model", "validateDays", "DAy invalide")
+		logger.Error("IssueLicenseUseCase", "Execute", AppError)
+		return AppError
+	}
+	return nil
+}
+
+func (license *License) GenerateFolio() {
+	license.Folio = fmt.Sprintf("L-%d", time.Now().Unix())
+}
+
+func (license *License) SetDefaultStatus(status string) {
+	license.Status = StatusIssued
+}
+
+func (l *License) IsValid() error {
+	if l.Days <= 0 {
+		AppError := err.NewAppError(err.ErrInternalError, "license model", "validateDays", "DAy invalide")
+		logger.Error("IssueLicenseUseCase", "Execute", AppError)
+		return AppError
+	}
+	if l.Status != StatusIssued {
+		AppError := err.NewAppError(err.ErrInternalError, "license model", "validateDays", "INVALIDED STATUS")
+		logger.Error("IssueLicenseUseCase", "Execute", AppError)
+		return AppError
+	}
+	return nil
 }
