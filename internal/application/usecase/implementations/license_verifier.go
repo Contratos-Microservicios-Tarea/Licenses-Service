71d525e1bcd2f53433b6f7bc1f82ad97c89f2c2d
Claudio Espinoza <claudioyugen@gmail.com>
2025-09-21 15:17:42 -0300

    feat: Implement LicenseVerifierUseCase with license verification logic and error handling

--- internal/application/usecase/implementations/license_verifier.go
+++ internal/application/usecase/implementations/license_verifier.go
@@ -3,16 +3,87 @@ package implementations
 import (
 	"context"
 	"license-service/internal/application/usecase/contrats"
+	"license-service/internal/domain/repositories"
+	errorInfo "license-service/pkg/log/error"
+	logger "license-service/pkg/log/logger"
 )
 
 type LicenseVerifierUseCase struct {
+	licenseRepository repositories.LicenseRepository
+	logger            logger.Logger
 }
 
-func NewLicenseVerifierUseCase() contrats.LicenseVerifier {
-	return &LicenseVerifierUseCase{}
+func NewLicenseVerifierUseCase(licenseRepository repositories.LicenseRepository) contrats.LicenseVerifier {
+	return &LicenseVerifierUseCase{
+		licenseRepository: licenseRepository,
+		logger:            *logger.NewLogger(),
+	}
 }
 
-// Execute implements contrats.LicenseVerifier.
 func (usecase *LicenseVerifierUseCase) Execute(ctx context.Context, folio string) (bool, error) {
-	panic("unimplemented")
+	usecase.logger.Info(
+		"LicenseVerifierUseCase",
+		"Execute",
+		"starting license verification process",
+	)
+
+	if err := usecase.validateFolio(folio); err != nil {
+		usecase.logger.Error(
+			"LicenseVerifierUseCase",
+			"Execute",
+			err,
+			"validation failed",
+		)
+		return false, err
+	}
+
+	license, err := usecase.licenseRepository.FindByFolio(ctx, folio)
+	if err != nil {
+		usecase.logger.Error(
+			"LicenseVerifierUseCase",
+			"Execute",
+			err,
+			"failed to retrieve license from repository",
+		)
+		return false, err
+	}
+
+	if license == nil {
+		usecase.logger.Info(
+			"LicenseVerifierUseCase",
+			"Execute",
+			"license not found",
+		)
+		return false, nil
+	}
+
+	isIssued := license.IsIssued()
+
+	if isIssued {
+		usecase.logger.Info(
+			"LicenseVerifierUseCase",
+			"Execute",
+			"license verification successful - valid license",
+		)
+	} else {
+		usecase.logger.Info(
+			"LicenseVerifierUseCase",
+			"Execute",
+			"license found but not issued",
+		)
+	}
+	return isIssued, nil
+}
+
+func (usecase *LicenseVerifierUseCase) validateFolio(folio string) error {
+	if folio == "" {
+		return errorInfo.NewAppError(
+			errorInfo.ErrMissingRequiredField,
+			"LicenseVerifierUseCase",
+			"validateFolio",
+			"folio is required",
+		)
+	}
+
+	return nil
 }
